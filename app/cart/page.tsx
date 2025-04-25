a4436c711a9b89313fb38474140ebdcb726a66ef
Tyler <tylerlarson919@gmail.com>
2025-04-25 12:26:42 -0400

    added /checkout

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useCart } from "@/context/CartContext";
 import { loadStripe } from "@stripe/stripe-js";
 
